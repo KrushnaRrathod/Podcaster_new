c37a1858270fc48efde861b28d4f4d2826b42060
Krushna Rathod <rathodkrushna2405@gmail.com>
2024-06-18 14:54:45 +0530

    commit

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -124,7 +124,7 @@ const GeneratePodcast = (props: GeneratePodcastProps) => {
       </div>
       {isAiAudio ? (
         <div>
-          <div className="flex flex-col gap-2.5">
+          <div className="mt-5 flex flex-col gap-2.5">
             <Label className="text-16 font-bold text-white-1">
               AI Prompt to generate Podcast
             </Label>
