ce75f015296b48d826624a59881ea35fbbb57013
Krushna Rathod <rathodkrushna2405@gmail.com>
2024-06-18 15:29:42 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET
   }
 };
 
